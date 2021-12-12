002f8149ed72e27c056a635c870efbcc0c3423e6
Anatoli Miske <hello@anatolimiske.de>
2021-12-12 20:50:00 +0100

    createCanvas works now

--- script.js
+++ script.js
@@ -3,13 +3,14 @@ function createCanvas(size) {
     const canvas = document.querySelector("#canvas")
 
     //append size-amount rows to canvas
-    for (i = 0; i < size; i++) {
+    for (i = 0; i < size*size; i++) {
         const div = document.createElement("div");
         div.classList.add("square");
+        div.style.height = ((canvas.offsetWidth-2)/size)-2+"px";
+        div.style.width = ((canvas.offsetWidth-2)/size)-2+"px";
         canvas.append(div);
-        console.log(canvas)
     }
-    //append size-amount divs into rows
+    console.log(canvas.offsetWidth);
 }
 
 createCanvas(10);
