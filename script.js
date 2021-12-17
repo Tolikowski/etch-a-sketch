175a13463332cd9627d9d45bdffc0cd8cef94ca5
Anatoli Miske <hello@anatolimiske.de>
2021-12-17 20:39:37 +0100

    rmoved borders and adjusted square calculations

--- script.js
+++ script.js
@@ -5,17 +5,17 @@ function createCanvas(size) {
     const canvas = document.querySelector("#canvas");
 
     //append size-amount rows to canvas
-    for (i = 0; i < size*size; i++) {
+    for (i = 0; i < size * size; i++) {
         const div = document.createElement("div");
         div.classList.add("square");
-        div.style.height = ((canvas.offsetWidth-2)/size)-2+"px";
-        div.style.width = ((canvas.offsetWidth-2)/size)-2+"px";
+        div.style.height = ((canvas.offsetWidth - 0) / size) - 0 + "px";
+        div.style.width = ((canvas.offsetWidth - 0) / size) - 0 + "px";
         canvas.append(div);
     }
 }
 
 //handle mouseenter and change background color of square
-function changeColor (color) {
+function changeColor(color) {
     document.querySelectorAll(".square").forEach(item => {
         item.addEventListener("mouseenter", event => {
             event.target.style.backgroundColor = color;
@@ -29,7 +29,7 @@ const resizeBtn = document.querySelector(".resize-btn");
 
 resizeBtn.addEventListener("click", event => {
     const newNumber = prompt("Please enter a number between 1 and 50.", "10");
-    if (newNumber > 50 || newNumber < 1){
+    if (newNumber > 50 || newNumber < 1) {
         alert("Error! Please enter a number between 1 and 50!")
     } else {
         document.querySelectorAll(".square").forEach(item => {
@@ -38,7 +38,7 @@ resizeBtn.addEventListener("click", event => {
         createCanvas(newNumber);
         changeColor(newColor);
     }
-    
+
 })
 
 
